15d4140b2f51b2a37f95192538f644569107480e
bibek-in-github <mahat6981@gmail.com>
2023-03-21 16:44:53 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require( __dirname + "/date.js")
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItems = [];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+
+  const day = date.getDate();
+ 
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  })
+});
+
+app.post("/", (req, res) => {
+
+  let item = req.body.newItem;
+  
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+    
+  } else {
+
+    items.push(item);
+    res.redirect("/");
+
+  }
+
+})
+
+app.get("/work", (req, res) => {
+
+  res.render("list", {
+    listTitle: "Work",
+    newListItems: workItems
+
+  })
+})
+
+app.post("/work", (rew, res) => {
+
+
+
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+
+})
+
+app.get("/about", (req, res) => {
+  res.render("about")
+})
+
+app.listen(3000, () => {
+  console.log("server is running at port 3000");
+});
+
+
